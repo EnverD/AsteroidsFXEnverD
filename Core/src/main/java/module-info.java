405131005b5e650d03adedaaedd9c48ee0957044
EnverD <116651134+EnverD@users.noreply.github.com>
2024-08-23 13:02:01 +0200

    Testing Unit, Junit

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module Core {
     requires Common;
     requires CommonBullet;
-    requires javafx.graphics;
+    //requires javafx.graphics;
     requires spring.context; // Ensure Spring Context is required
-    requires spring.beans;   // Ensure Spring Beans is required
+    requires spring.beans;
+    requires javafx.graphics;   // Ensure Spring Beans is required
     opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core; // Adjust as necessary
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
